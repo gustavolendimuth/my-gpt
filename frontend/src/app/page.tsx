c83a9c4e369810cbfe0a3a4052688e8be2b72997
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-06-28 22:03:23 -0300

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-6xl font-bold">My GPT</h1>
+      <p className="text-2xl">my personal assistant</p>
+    </main>
+  )
+}
